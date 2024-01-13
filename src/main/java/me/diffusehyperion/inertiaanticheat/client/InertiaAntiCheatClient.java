c78643f7a325bd5f074839d923b80a81bf244101
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2024-01-13 21:04:11 +0800

    minor bugfixes

--- src/main/java/me/diffusehyperion/inertiaanticheat/client/InertiaAntiCheatClient.java
+++ src/main/java/me/diffusehyperion/inertiaanticheat/client/InertiaAntiCheatClient.java
@@ -1,29 +1,18 @@
 package me.diffusehyperion.inertiaanticheat.client;
 
 import com.moandjiezana.toml.Toml;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GenericFutureListener;
 import me.diffusehyperion.inertiaanticheat.InertiaAntiCheat;
 import me.diffusehyperion.inertiaanticheat.util.InertiaAntiCheatConstants;
 import me.diffusehyperion.inertiaanticheat.util.Scheduler;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientLoginNetworking;
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientLoginNetworkHandler;
-import net.minecraft.network.PacketByteBuf;
 
 import java.io.File;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.nio.file.Files;
-import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
 
 public class InertiaAntiCheatClient implements ClientModInitializer {
     public static Toml clientConfig;
