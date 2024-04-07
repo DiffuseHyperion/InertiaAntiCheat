bba2987240aeac5fdfb3638185741d115df12239
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2024-04-07 19:40:08 +0800

    Push to 1.0.2.1
    Mod no longer creates new query packet listener for server side, instead mixing into vanilla listener for mod compatability

---
+++ src/main/java/me/diffusehyperion/inertiaanticheat/mixins/server/ServerQueryNetworkHandlerMixin.java
@@ -0,0 +1,41 @@
+package me.diffusehyperion.inertiaanticheat.mixins.server;
+
+import me.diffusehyperion.inertiaanticheat.packets.S2C.AnticheatDetailsS2CPacket;
+import me.diffusehyperion.inertiaanticheat.server.InertiaAntiCheatServer;
+import me.diffusehyperion.inertiaanticheat.util.GroupAnticheatDetails;
+import me.diffusehyperion.inertiaanticheat.util.IndividualAnticheatDetails;
+import me.diffusehyperion.inertiaanticheat.util.ModlistCheckMethod;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.packet.c2s.query.QueryPingC2SPacket;
+import net.minecraft.server.network.ServerQueryNetworkHandler;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerQueryNetworkHandler.class)
+public abstract class ServerQueryNetworkHandlerMixin {
+    @Shadow @Final
+    private ClientConnection connection;
+
+    @Inject(method = "onQueryPing",
+    at = @At(value = "HEAD"))
+    private void injectSendAnticheatDetails(QueryPingC2SPacket packet, CallbackInfo ci) {
+        if (InertiaAntiCheatServer.modlistCheckMethod == ModlistCheckMethod.INDIVIDUAL) {
+            IndividualAnticheatDetails details =
+                    new IndividualAnticheatDetails(
+                            InertiaAntiCheatServer.serverConfig.getBoolean("motd.showInstalled"),
+                            InertiaAntiCheatServer.serverConfig.getList("motd.blacklist"),
+                            InertiaAntiCheatServer.serverConfig.getList("motd.whitelist"));
+            this.connection.send(new AnticheatDetailsS2CPacket(details));
+        } else {
+            GroupAnticheatDetails details =
+                    new GroupAnticheatDetails(
+                            InertiaAntiCheatServer.serverConfig.getBoolean("motd.showInstalled"),
+                            InertiaAntiCheatServer.serverConfig.getList("motd.hash"));
+            this.connection.send(new AnticheatDetailsS2CPacket(details));
+        }
+    }
+}
--- src/main/java/me/diffusehyperion/inertiaanticheat/packets/ServerUpgradedQueryPacketListener.java
+++
@@ -1,6 +0,0 @@
-package me.diffusehyperion.inertiaanticheat.packets;
-
-import net.minecraft.network.listener.ServerQueryPacketListener;
-
-public interface ServerUpgradedQueryPacketListener extends ServerQueryPacketListener {
-}
