6ebb5c88dcedf9a5e757be6bce7aab7347473638
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2024-10-31 23:25:10 +0800

    update to 1.21.3

--- src/main/java/me/diffusehyperion/inertiaanticheat/mixins/client/MultiplayerServerListWidgetServerEntryMixin.java
+++ src/main/java/me/diffusehyperion/inertiaanticheat/mixins/client/MultiplayerServerListWidgetServerEntryMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
 import net.minecraft.client.network.ServerInfo;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
@@ -49,7 +50,7 @@ public abstract class MultiplayerServerListWidgetServerEntryMixin {
         if (Objects.nonNull(installed) && installed.equals(true) && anticheatDetails.showInstalled()) {
             int iconX = x + entryWidth - 15;
             int iconY = y + 10;
-            context.drawTexture(ICON_ENABLED, iconX, iconY, 0.0f, 0.0f, 10, 10, 10, 10);
+            context.drawTexture(RenderLayer::getGuiTextured, ICON_ENABLED, iconX, iconY, 0.0f, 0.0f, 10, 10, 10, 10);
             if (mouseX > iconX && mouseX < iconX + 10 && mouseY > iconY && mouseY < iconY + 10) {
                 screen.setTooltip(Text.of("InertiaAntiCheat installed"));
             }
@@ -61,7 +62,7 @@ public abstract class MultiplayerServerListWidgetServerEntryMixin {
                 if ((details.getWhitelistedMods().size() == 1 && !Objects.equals(details.getWhitelistedMods().getFirst(), "")) || details.getWhitelistedMods().size() >= 2) {
                     int whitelistIconX = x + entryWidth - 25;
                     int whitelistIconY = y + 20;
-                    context.drawTexture(ICON_WHITELIST, whitelistIconX, whitelistIconY, 0.0f, 0.0f, 10, 10, 10, 10);
+                    context.drawTexture(RenderLayer::getGuiTextured, ICON_WHITELIST, whitelistIconX, whitelistIconY, 0.0f, 0.0f, 10, 10, 10, 10);
                     if (mouseX > whitelistIconX && mouseX < whitelistIconX + 10 && mouseY > whitelistIconY && mouseY < whitelistIconY + 10) {
                         screen.setTooltip(details.getWhitelistedMods().stream().map(Text::of).map(Text::asOrderedText).toList());
                     }
@@ -70,7 +71,7 @@ public abstract class MultiplayerServerListWidgetServerEntryMixin {
                 if ((details.getBlacklistedMods().size() == 1 && !Objects.equals(details.getBlacklistedMods().getFirst(), "")) || details.getBlacklistedMods().size() >= 2) {
                     int blacklistIconX = x + entryWidth - 15;
                     int blacklistIconY = y + 20;
-                    context.drawTexture(ICON_BLACKLIST, blacklistIconX, blacklistIconY, 0.0f, 0.0f, 10, 10, 10, 10);
+                    context.drawTexture(RenderLayer::getGuiTextured, ICON_BLACKLIST, blacklistIconX, blacklistIconY, 0.0f, 0.0f, 10, 10, 10, 10);
                     if (mouseX > blacklistIconX && mouseX < blacklistIconX + 10 && mouseY > blacklistIconY && mouseY < blacklistIconY + 10) {
                         screen.setTooltip(details.getBlacklistedMods().stream().map(Text::of).map(Text::asOrderedText).toList());
                     }
@@ -80,7 +81,7 @@ public abstract class MultiplayerServerListWidgetServerEntryMixin {
                 if ((details.getModpackDetails().size() == 1 && !Objects.equals(details.getModpackDetails().getFirst(), "")) || details.getModpackDetails().size() >= 2) {
                     int blacklistIconX = x + entryWidth - 15;
                     int blacklistIconY = y + 20;
-                    context.drawTexture(ICON_MODPACK, blacklistIconX, blacklistIconY, 0.0f, 0.0f, 10, 10, 10, 10);
+                    context.drawTexture(RenderLayer::getGuiTextured, ICON_MODPACK, blacklistIconX, blacklistIconY, 0.0f, 0.0f, 10, 10, 10, 10);
                     if (mouseX > blacklistIconX && mouseX < blacklistIconX + 10 && mouseY > blacklistIconY && mouseY < blacklistIconY + 10) {
                         screen.setTooltip(details.getModpackDetails().stream().map(Text::of).map(Text::asOrderedText).toList());
                     }
